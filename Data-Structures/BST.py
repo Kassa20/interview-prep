be92077ccc7671bb4884ab79a264053435c585e4
neo <ykassa47@gmail.com>
2025-09-12 11:05:06 -0700

    Implemented BST

---
+++ Data-Structures/BST.py
@@ -0,0 +1,60 @@
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+class BST:
+    def __init__(self, root=None):
+        self.root = root
+
+
+    def insert(self, data):
+
+        if self.root == None:
+            self.root = TreeNode(value=data)
+            return
+
+        curr = self.root
+        while True:
+
+            if data > curr.value:
+                if curr.right == None:
+                    curr.right = TreeNode(data)
+                    break
+                curr = curr.right
+            
+            else:
+                if curr.left == None:
+                    curr.left = TreeNode(data)
+                    break
+        
+    
+    
+    def search(self, data):
+
+        curr = self.root
+
+        while curr != None:
+            if data > curr.value:
+                curr = curr.right
+            elif data < curr.value:
+                curr = curr.left
+            else:
+                return True
+        
+        return False
+
+
+    def preOrder(self, root):
+
+        if root == None:
+            return
+        
+        print(root.val)
+        self.preOrder(root.left)
+        self.preOrder(root.right)
+
+        return 
+        
